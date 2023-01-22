a81a9a5881cd3e03dd36973c8f8d57c17f284015
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2023-01-22 17:23:01 +1000

    Try use "exec" correctly

--- index.js
+++ index.js
@@ -5,20 +5,9 @@ const child_process = require("child_process")
 try {
     const testFiles = core.getInput("test-files")
 
-    child_process.exec(`ls --all`, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`)
-            return
-        }
-        console.log(`stdout: ${stdout}`)
-        console.error(`stderr: ${stderr}`)
-    })
-
-    return
-
     glob(testFiles, {}, (_er, files) => {
         const filesAsString = files.map(file => `"${file}"`).join(" ")
-        child_process.exec(`bash main.sh ${filesAsString}`, (error, stdout, stderr) => {
+        child_process.exec(`sudo apt update -y && sudo apt install emacs && for file in ${filesAsString}; do emacs -batch -l ert -l "\${file}" -f ert-run-tests-batch-and-exit; done`, (error, stdout, stderr) => {
             if (error) {
                 console.error(`exec error: ${error}`)
                 return
