e356038408196a7142c2d9eca614e8b7100b45d3
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2023-01-22 17:16:11 +1000

    Just print all files

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ const child_process = require("child_process")
 try {
     const testFiles = core.getInput("test-files")
 
+    const filesAsString = files.map(file => `"${file}"`).join(" ")
+    child_process.exec(`ls --all`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`)
+            return
+        }
+        console.log(`stdout: ${stdout}`)
+        console.error(`stderr: ${stderr}`)
+    })
+
+    return
+
     glob(testFiles, {}, (_er, files) => {
-        const filesAsString = files.map(file => `"${file}"`).join(" ")
-        child_process.exec(`ls --all`, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`)
-                return
-            }
-            console.log(`stdout: ${stdout}`)
-            console.error(`stderr: ${stderr}`)
-        })
         child_process.exec(`bash main.sh ${filesAsString}`, (error, stdout, stderr) => {
             if (error) {
                 console.error(`exec error: ${error}`)
