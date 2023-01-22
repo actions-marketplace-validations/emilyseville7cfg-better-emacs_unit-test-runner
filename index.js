5cf93860fafa9b537b0ddfa593a82fa00fd6c1c3
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2023-01-22 15:43:28 +1000

    Run ERT tests

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const core = require("@actions/core");
+const core = require("@actions/core")
 const glob = require("glob")
+const child_process = require("child_process")
+
+function executeShellCommand(command) {
+    child_process.exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`)
+            return
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`)
+            return
+        }
+        console.log(`stdout: ${stdout}`)
+    })
+}
 
 try {
-    const testFiles = core.getInput("test-files");
+    const testFiles = core.getInput("test-files")
+
+    executeShellCommand("sudo apt update -y && sudo apt install emacs")
 
     glob(testFiles, {}, (_er, files) => {
-        files.forEach(file => console.log(`${file} matches`))
-    });
+        files.forEach(file => executeShellCommand(`emacs -batch -l ert -l "${file}" -f ert-run-tests-batch-and-exit`))
+    })
 } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
 }
